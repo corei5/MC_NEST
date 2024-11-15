5194ed99dd1dbeedf559175b5190c71a56374c27
gollam rabby <rabby2186@gmail.com>
2024-11-15 11:22:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
-    name='mcts_package',
-    version='0.1',
-    description='A package to run MCTS with GPT-4o integration for problem-solving',
-    author='Gollam Rabby',
-    author_email='your_email@example.com',
+    name="MC-NEST-GPT",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        'openai',  # Add other dependencies here
-        'pydantic',
-        'numpy',
-        'pandas',
-        'tqdm',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        "openai",
+        "pandas",
+        "numpy",
+        "pydantic",
+        "tqdm",
     ],
 )
-
