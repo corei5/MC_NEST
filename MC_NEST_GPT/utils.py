df35f0cec9c83bc88031c812ebcb043d37c29319
gollam rabby <rabby2186@gmail.com>
2024-11-15 11:44:32 +0100

    Update utils.py

--- MC_NEST_GPT/utils.py
+++ MC_NEST_GPT/utils.py
@@ -1,12 +1,12 @@
-# mcts/utils.py
+# # mcts/utils.py
 
-def print_tree(node: Node | None, level: int = 0):
-    if node is None:
-        return
-    indent = " " * level * 2
-    node_str = repr(node)
-    for line in node_str.split("\n"):
-        print(indent + line)
-    for child in node.children:
-        print_tree(child, level + 1)
+# def print_tree(node: Node | None, level: int = 0):
+#     if node is None:
+#         return
+#     indent = " " * level * 2
+#     node_str = repr(node)
+#     for line in node_str.split("\n"):
+#         print(indent + line)
+#     for child in node.children:
+#         print_tree(child, level + 1)
 
