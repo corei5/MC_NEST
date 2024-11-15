8c37233f814eae42f5eef7a742147f88a4d255a2
gollam rabby <rabby2186@gmail.com>
2024-11-15 11:57:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",  # Specify the version here
     packages=find_packages(),
     install_requires=[
-        "openai>=0.27.0",   # Replace with the latest stable version
+        "openai>=1.54.3",   # Replace with the latest stable version
         "pandas>=1.5.0",    # Replace with the latest stable version
         "numpy>=1.24.0",    # Replace with the latest stable version
         "pydantic>=2.0.0",  # Replace with the latest stable version
